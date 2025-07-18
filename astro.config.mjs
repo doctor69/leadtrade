abdc22ff4bc4102f325de2a147732acf8cce0bf2
doctor69 <31432011+doctor69@users.noreply.github.com>
2025-07-18 13:13:04 -0400

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  integrations: [react(), tailwind()],
 
   build: {
     inlineStylesheets: 'auto'
